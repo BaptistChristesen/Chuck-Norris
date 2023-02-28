2c0c1cef68c646d17e4c8c81540e529ccc8b0513
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-02-28 11:06:42 -0600

    Initial Commit

---
+++ Chuck Norris/Chuck_NorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chuck_NorrisApp.swift
+//  Chuck Norris
+//
+//  Created by caden christesen on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Chuck_NorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
